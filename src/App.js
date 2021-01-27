e9c1bdfa948b41c42794eb55d2b6aba51c1549c2
German G Sayago <german.sayago@gmail.com>
2021-01-27 14:38:56 -0300

    se instalaron dependencias y se creo la primer navegacion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Tanks from './pages/Tanks';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/tanks'>Tanks</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/tanks'>
+            <Tanks />
+          </Route>
+          <Route path='*'>
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Tanks.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function tanks() {
+  return (
+    <div>
+      <h1>Tanks</h1>
+    </div>
+  );
+}
